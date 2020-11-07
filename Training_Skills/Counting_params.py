600165a6b946baffd93a711cd7eef95cb638d77c
rentianhe <596106517@qq.com>
2020-11-07 21:19:50 +0800

    add print parameters

--- Training_Skills/Counting_params.py
+++ Training_Skills/Counting_params.py
@@ -5,6 +5,15 @@ import torch.nn.functional as F
 '''
     counting the params whose requires_grad is True
 '''
-def count_parameters(model):
-    params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    return params/1000000
+# Version 1
+def count_parameters(net):
+    params = sum([param.nelement() for param in net.parameters() if param.requires_grad])
+    print("Params: %f M" % (params/1000000))
+
+# Version 2
+def print_parameters(model):
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f'{total_params:,} total parameters.')
+    total_trainable_params = sum(
+        p.numel() for p in model.parameters() if p.requires_grad)
+    print(f'{total_trainable_params:,} training parameters.')

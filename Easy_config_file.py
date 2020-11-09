e779b7c13f6036e492056e964b47c1bb27f29721
rentianhe <596106517@qq.com>
2020-11-09 23:15:15 +0800

    add config file builder

---
+++ Easy_config_file.py
@@ -0,0 +1,21 @@
+import ml_collections
+
+def get_b16_config():
+    """Returns the ViT-B/16 configuration."""
+    config = ml_collections.ConfigDict()
+    config.patches = ml_collections.ConfigDict({'size': (16, 16)})
+    config.hidden_size = 768
+    config.transformer = ml_collections.ConfigDict()
+    config.transformer.mlp_dim = 3072
+    config.transformer.num_heads = 12
+    config.transformer.num_layers = 12
+    config.transformer.attention_dropout_rate = 0.0
+    config.transformer.dropout_rate = 0.1
+    config.classifier = 'token'
+    config.representation_size = None
+    config.SparseRandomDrop = True
+    config.use_fixed_position_embeddings = True
+    return config
+
+config =  get_b16_config()
+print(config)

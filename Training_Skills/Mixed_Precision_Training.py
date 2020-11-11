627c2a754ca9d7128b259dd84cde606a5d21149d
rentianhe <596106517@qq.com>
2020-11-11 23:10:14 +0800

    add mixed precision training method

--- Training_Skills/Mixed_Precision_Training.py
+++ Training_Skills/Mixed_Precision_Training.py
@@ -17,7 +17,9 @@ model = model.zero_grad()
 #   pred = model(x)
 #   with torch.cuda.amp.autocast(): # Second
 #       loss = loss_fn(pred, label)
-#       scalar.scale(loss).backward(retain_graph = True) # Third
+#       scalar.scale(loss).backward() # Third
+#       # if there are something wrong you can try this
+#       # scalar.scale(loss).backward(retain_graph = True)
 
 #   scalar.step(optimizer)
 #   scalar.update()
